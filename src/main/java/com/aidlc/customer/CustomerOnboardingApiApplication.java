fc6f6b287ceffd5a6a8c032274b825a183a0ca4a
Bhaskar Dasari <bhaskardasari3004@gmail.com>
2026-06-18 14:54:20 +0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/aidlc/customer/CustomerOnboardingApiApplication.java
@@ -0,0 +1,13 @@
+package com.aidlc.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOnboardingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOnboardingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aidlc/customer/CustomerOnboardingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidlc.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerOnboardingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
